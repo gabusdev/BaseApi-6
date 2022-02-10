85ec0292aed2969e4f76e71e71fc4ec7624fcdcd
gabusdev <gabusdev@outlook.com>
2022-02-09 23:29:16 -0500

    Added Logging

---
+++ BaseApi.Api/AppServices/FluentValidation/FluentValidationExtension.cs
@@ -0,0 +1,16 @@
+﻿using BaseApi.Core.Validations;
+using Common.DTO.Request.Validations;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BaseApi.Api.AppServices.FluentValidation
+{
+    public static class FluentValidationExtension
+    {
+        public static void ConfigureFluentValidation(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<UserValidator>();
+            services.AddValidatorsFromAssemblyContaining<LoginDTOValidator>();
+        }
+    }
+}
---
+++ BaseApi.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using BaseApi.Core.Entities;
+using BaseApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace BaseApi.Api.Controllers
+{
+    [ApiVersion("1.0", Deprecated = true)] // Version 1, deprecated
+    [ApiController]
+    [Route("forecast")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IWeatherForecastService _service;
+
+        public WeatherForecastController(IWeatherForecastService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<WeatherForecast> Get()
+        {
+            return Ok(_service.GetForecast());
+        }
+
+        [HttpGet("/greet")]
+        public ActionResult<string> Greet()
+        {
+            Log.Information("Greeted from V 1.0");
+            return Ok(_service.Greeting() + " from V1");
+        }
+    }
+
+
+}
